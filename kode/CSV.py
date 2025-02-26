e439d807d877635dd748aaa993b91600466af6ee
Andreas Roost <andreasroost14@gmail.com>
2025-02-26 11:14:20 +0100

    csv

--- kode/CSV.py
+++ kode/CSV.py
@@ -7,6 +7,6 @@ df = pd.read_excel(excel_file_path, sheet_name=0)  # Loads the first sheet
 
 # Save it as a new CSV file
 csv_file_path = "C:/Users/Andre/OneDrive/Dokumenter/bachelor/bachelor/Data.csv"  # Define your output CSV file name
-df.to_csv(csv_file_path, index=False, encoding='utf-8')  # Saves without index and in UTF-8 encoding
+df.to_csv(csv_file_path, index=False, encoding='utf-8', sep=';')
 
 print(f"Excel file has been successfully converted to: {csv_file_path}")
